b9908461d83ae9ed392cea9555cb767e37be38f9
anshu-shagunmishra <shagunmishra133@gmail.com>
2026-07-15 17:10:58 +0530

    Setup SQLAlchemy database foundation

--- backend/database/connection.py
+++ backend/database/connection.py
@@ -2,16 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from config.settings import DATABASE_URL
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, echo=True)
 
 SessionLocal = sessionmaker(
-<<<<<<< HEAD
-    autocommit = False,
-    autoflush = False,
-    bind = engine
-=======
     autocommit=False,
     autoflush=False,
     bind=engine
->>>>>>> develop
 )
